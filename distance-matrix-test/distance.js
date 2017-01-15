c6d8f40793c758428388257a86702a6cb9da15fb
chinjon <Jonathankchin@gmail.com>
2017-01-15 13:16:42 -0500

    extacting data from JSON data

--- distance-matrix-test/distance.js
+++ distance-matrix-test/distance.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var distance = require('google-distance-matrix');
 // AIzaSyAhVCu_gr8RKRpyAtvWqbtRb-DFyCvgqUM
 
-
 var GOOGLE_CLIENT_KEY = 'AIzaSyAhVCu_gr8RKRpyAtvWqbtRb-DFyCvgqUM';
 
 
@@ -12,9 +11,20 @@ var googleMapsClient = require('@google/maps').createClient({
 
 
 var origins = ['Yonkers, NY '];
-var destinations = ['New York NY', '40.7644882,-73.98246'];
+var destinations = [
+    'New York NY', 
+    '40.7644882,-73.98246'
+    
+    ];
 
-distance.matrix(origins, destinations, function (err, distances) {
-    if (!err)
-        fs.appendFileSync('distance-test.json', JSON.stringify(distances));
+distance.matrix(origins, destinations, function (err, data) {
+  if (!err)
+
+    var locationOne = data.rows[0].elements[0];
+    var locationTwo = data.rows[0].elements[1];
+    console.log("Distance to location one: " + JSON.stringify(locationOne.distance.text));
+    console.log("Travel time to location one: "+ JSON.stringify(locationOne.duration.value));
+    console.log("Distance to location two: " + JSON.stringify(locationTwo.distance.text));
+    console.log("Travel time to location two: "+ JSON.stringify(locationTwo.duration.value));
+    //fs.writeFile('distance-test.json', JSON.stringify(data));
 });
